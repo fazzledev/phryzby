71edd6ac819e3f74af615363513dc26d1374118d
Syed Fazil Basheer <syedfazilbasheer@gmail.com>
2026-09-21 06:55:29 +0530

    Say refractive index rather than index

--- lib/light/optics.rb
+++ lib/light/optics.rb
@@ -4,15 +4,15 @@ module Optics
   extend Physics::Quantities
 
   RIGHT_ANGLE = 0.0..(Math::PI / 2)
-  INDEX = 1.0..4.0
+  REFRACTIVE_INDEX = 1.0..4.0
   FRACTION = 0.0..1.0
 
   variable :angle_of_incidence,                alias: :i,   within: RIGHT_ANGLE
   variable :angle_of_reflection,               alias: :rl,  within: RIGHT_ANGLE
   variable :angle_of_refraction,               alias: :rr,  within: RIGHT_ANGLE
 
-  variable :refractive_index_of_first_medium,  alias: :mu1, within: INDEX
-  variable :refractive_index_of_second_medium, alias: :mu2, within: INDEX
+  variable :refractive_index_of_first_medium,  alias: :mu1, within: REFRACTIVE_INDEX
+  variable :refractive_index_of_second_medium, alias: :mu2, within: REFRACTIVE_INDEX
 
   variable :reflectance_s_polarised, alias: :rs, within: FRACTION
   variable :reflectance_p_polarised, alias: :rp, within: FRACTION
